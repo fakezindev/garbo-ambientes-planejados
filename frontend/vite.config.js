1447c203aa2af65dc51d20f1ae5d841d45bcc053
fakezindev <zetabr.bruno@gmail.com>
2026-03-01 20:58:18 -0300

    ci: configura deploy automatico do frontend para o github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'garbo-ambientes-planejados',
   server: {
     host: true, // Isso libera o acesso externo (0.0.0.0)
     strictPort: true,
