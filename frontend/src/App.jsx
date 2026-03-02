0d897c799ff8e23829f2ea4eee864e5e6ccc3f00
fakezindev <zetabr.bruno@gmail.com>
2026-03-02 11:29:21 -0300

    fix: ajusta nome do BrowserRouter para o github pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router basename="/garbo-ambientes-planejados">
+    <BrowserRouter basename="/garbo-ambientes-planejados">
         <Routes>
           {/* ROTA PÚBLICA: Landing Page */}
           <Route path="/" element={<Home />} />
@@ -58,7 +57,6 @@ function App() {
           pauseOnHover
           theme="dark" // Pode mudar para "light" ou "colored" se preferir
         />
-      </Router>
     </BrowserRouter>
   );
 }
